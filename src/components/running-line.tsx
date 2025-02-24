1c9dd7cc7e2e9de827a6caa39d0f9d0adf04075a
Marina <pchupchu@yandex.ru>
2025-02-24 19:20:32 +0300

    Fix running line

--- src/components/running-line.tsx
+++ src/components/running-line.tsx
@@ -13,14 +13,14 @@ interface RunningLineProps {
 
 const RunningLine = ({ xDirection, textStyle }: RunningLineProps) => {
   return (
-    <div className='overflow-x-hidden'>
-      <motion.div
-        className='flex'
-        initial={{ x: 0 }}
-        animate={{ x: xDirection }}
-        transition={{ duration: 5, repeat: Infinity, ease: 'linear' }}>
-        {Array.from({ length: 2 }).map((_, i) => (
-          <Fragment key={i}>
+    <div className='flex selection:overflow-x-hidden'>
+      {Array.from({ length: 2 }).map((_, i) => (
+        <Fragment key={i}>
+          <motion.div
+            className='flex'
+            initial={{ x: 0 }}
+            animate={{ x: xDirection }}
+            transition={{ duration: 20, repeat: Infinity, ease: 'linear' }}>
             <p
               className={cn(
                 'text-nowrap px-4 text-[93px] font-medium',
@@ -28,9 +28,9 @@ const RunningLine = ({ xDirection, textStyle }: RunningLineProps) => {
               )}>
               Data driven user focused value based
             </p>
-          </Fragment>
-        ))}
-      </motion.div>
+          </motion.div>
+        </Fragment>
+      ))}
     </div>
   );
 };
