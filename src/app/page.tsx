5631763f889d09ffcc8b3c2f80e11b892d75fdf4
Marina <pchupchu@yandex.ru>
2025-02-24 11:24:28 +0300

    Add mobile hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import Hero from './components/hero';
+
 export default function Home() {
   return (
     <div className='font-primary'>
-      <main className=''></main>
+      <main className=''>
+        <div className='p-1'>
+          <Hero />
+        </div>
+      </main>
       <footer className=''>footer</footer>
     </div>
   );
