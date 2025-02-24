84d2fbb2d19b1b518512fcd40425ac1501222504
Marina <pchupchu@yandex.ru>
2025-02-24 11:07:02 +0300

    Add shadcn library

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,41 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
+  darkMode: ['class'],
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '15px',
       },
     },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '960px',
+      xl: '1200px',
+    },
+    fontFamily: {
+      primary: 'var(--font-inter)',
+      secondary: 'var(--font-jakarta)',
+      accent: 'var(--font-freigeist-regular)',
+      accent_bold: 'var(--font-freigeist-bold)',
+      accent_italic: 'var(--font-freigeist-bold-italic)',
+    },
+    extend: {
+      colors: {
+        background: '#ffffff',
+        foreground: '#000000',
+
+        border: 'hsl(var(--border))',
+      },
+      backgroundImage: {},
+    },
   },
-  plugins: [],
+  plugins: [require('tailwindcss-animate')],
 } satisfies Config;
