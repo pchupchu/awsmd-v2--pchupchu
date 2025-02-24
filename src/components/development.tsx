ad4232d7fdde7b50a1990a45c55b660b2d7c54da
Marina <pchupchu@yandex.ru>
2025-02-24 19:12:56 +0300

    Remove folder with components

---
+++ src/components/development.tsx
@@ -0,0 +1,18 @@
+const Development = () => {
+  return (
+    <section>
+      <div className='container relative bg-[#0D0F11] pt-8 text-white'>
+        <div className='mb-5 flex justify-between gap-4 uppercase leading-tight'>
+          <h3 className='text-[32px]'>&#123;smart&#125; development</h3>
+          <span className='font-secondary text-[44px] text-white/40'>**</span>
+        </div>
+        <p className='text-[14px] leading-4 tracking-tight text-white/85'>
+          Making your business outstanding is&nbsp;a&nbsp;science.
+          We&nbsp;take&nbsp;it (a) seriously and (b) creatively.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Development;
