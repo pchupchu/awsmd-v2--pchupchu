8796127cbbd90dba8d0375523cbe14c901149c23
Marina <pchupchu@yandex.ru>
2025-03-08 21:00:31 +0300

    Fix portfolio

---
+++ src/utils/data.ts
@@ -0,0 +1,37 @@
+export const projects = [
+  {
+    id: 1,
+    title: 'SpinApp',
+    description:
+      'Web Development for Web3 Gaming Platform with community-driven assets, decentralized economy and gamified experience',
+    image: '/projects/spinapp.png',
+  },
+  {
+    id: 2,
+    title: 'Bottle-Haus',
+    description:
+      'Website Development for Bottle-Haus - Leading Online Retailer of Fine Alcoholic Spirits',
+    image: '/projects/bottle-haus.png',
+  },
+  {
+    id: 3,
+    title: 'UI UX Design And Mobile Development for Fintech App',
+    description:
+      'In Syfter App,we made it easy for people to invest into crypto and stock market assets with its user friendly interface and interactive swiping experience',
+    image: '/projects/syfter.jpg',
+  },
+  {
+    id: 4,
+    title: 'Mobile App Design and Development for NFT Marketplace',
+    description:
+      'We created slick design for nft app that brings nft market right into users smartphone allowing them to trade tokens and share with their friends',
+    image: '/projects/vvs.jpg',
+  },
+  {
+    id: 5,
+    title: 'Modern Design And Development of Ecommerce App',
+    description:
+      'Retlify - is an ecommerce app that broadens limits of users and sellers by offering them interactive and innovative shopping experience',
+    image: '/projects/ritilo.jpg',
+  },
+];
