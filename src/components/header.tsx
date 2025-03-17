60bf1aa7876d7f53b20a0091f94cb845902ff0bc
Marina <pchupchu@yandex.ru>
2025-03-17 17:05:24 +0300

    Fix header z-index

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ import Logo from './logo';
 
 const Header = () => {
   return (
-    <header className='absolute left-0 right-0 top-0 flex w-full items-center justify-between px-4 pt-6'>
+    <header className='absolute left-0 right-0 top-0 z-50 flex w-full items-center justify-between px-4 pt-6'>
       <Logo imageSrc='/icons/logo-light.svg' textStyles='text-white' />
       <button
         className='fixed right-4 top-3 flex h-12 w-12 items-center justify-center rounded-full bg-white/40'
